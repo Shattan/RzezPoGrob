f0a9d89757eb3d94adc3f94dde8189de8d3d0731
Shattan <rosiak90@gmail.com>
2015-10-30 23:02:30 +0100

    Revert "PrzyciskiPrzeniesione"
    This reverts commit 5d02670040ef7a04b27f14bce28c2829572096a1.

--- RPG/Tla/EkranGryTlo.cs
+++ RPG/Tla/EkranGryTlo.cs
@@ -19,7 +19,7 @@ namespace RPG
 
         #region Zmienne
         EkranGry ekranGry;
-        PictureBox[] praweMenu;
+
         #endregion
 
         public EkranGryTlo(EkranGry ekranGry)
@@ -49,71 +49,12 @@ namespace RPG
             Width = Screen.PrimaryScreen.Bounds.Width;
             Height = Screen.PrimaryScreen.Bounds.Height;
 
-            //Wczytanie Right Menu Panel
-            List<Image> ListaObrazkow = new List<Image>();
-            ListaObrazkow.Add(new Bitmap("Resources/Grafiki menu/Adark.png"));
-            ListaObrazkow.Add(new Bitmap("Resources/Grafiki menu/Bdark.png"));
-            ListaObrazkow.Add(new Bitmap("Resources/Grafiki menu/Cdark.png"));
-            ListaObrazkow.Add(new Bitmap("Resources/Grafiki menu/D.png"));
-            ListaObrazkow.Add(new Bitmap("Resources/Grafiki menu/E.png"));
-
-            const int wielkoscPrzyciskow = 80;
-            const int odlegloscMiedzyPrzyciskami = 20;
-            int iloscPrzyciskow = ListaObrazkow.Count();
-
-            panelPraweMenu.Location = new Point(Screen.PrimaryScreen.Bounds.Width - wielkoscPrzyciskow, Screen.PrimaryScreen.Bounds.Y);
-            panelPraweMenu.Size = new Size(wielkoscPrzyciskow + odlegloscMiedzyPrzyciskami, wielkoscPrzyciskow * iloscPrzyciskow);
-
-            Program.UstawObrazZDopasowaniemWielkosciObrazuDoKontrolki(panelPraweMenu, "Resources/Grafiki menu/Panel pod przyciski.png");
-            praweMenu = new PictureBox[iloscPrzyciskow];
-            for (int index = 0; index < praweMenu.Length; index++)
-            {
-                praweMenu[index] = new PictureBox();
-                panelPraweMenu.Controls.Add(praweMenu[index]);
-                praweMenu[index].Location = new Point(0, index * wielkoscPrzyciskow + odlegloscMiedzyPrzyciskami);
-                praweMenu[index].Size = new Size(wielkoscPrzyciskow, wielkoscPrzyciskow);
-                praweMenu[index].BackgroundImage = new Bitmap(ListaObrazkow[index], wielkoscPrzyciskow, wielkoscPrzyciskow);
-                //praweMenu[index].MouseEnter += new System.EventHandler(this.PictureBoxPraweMenu_MouseEnter);
-                //praweMenu[index].MouseLeave += new System.EventHandler(this.PictureBoxPraweMenu_MouseLeave);
-            }
-
-            praweMenu[0].Click += new System.EventHandler(this.PictureBoxPraweMenuEkwipunek_MouseClick);
-            praweMenu[0].MouseEnter += new System.EventHandler(this.PictureBoxPraweMenuEkwipunek_MouseEnter);
-            praweMenu[0].MouseLeave += new System.EventHandler(this.PictureBoxPraweMenuEkwipunek_MouseLeave);
-            praweMenu[1].Click += new System.EventHandler(this.PictureBoxPraweMenuEkranDziennikZadan_MouseClick);
-            //praweMenu[2].Click += new System.EventHandler(this.PictureBoxPraweMenuEkwipunek_MouseClick);
-            //praweMenu[3].Click += new System.EventHandler(this.PictureBoxPraweMenuEkwipunek_MouseClick);
-
-            PictureBoxTrawa.Size = new Size(plansza.Width,plansza.Height);
-            PictureBoxTrawa.Location = new Point(0-plansza.Width/2,0-plansza.Height/2);
-            PictureBoxTrawa.Image = plansza;
+             PictureBoxTrawa.Size = new Size(plansza.Width,plansza.Height);
+             PictureBoxTrawa.Location = new Point(0-plansza.Width/2,0-plansza.Height/2);
+             PictureBoxTrawa.Image = plansza;
 
             //PictureBoxMgla.Image = new Bitmap("Resources/Grafiki gracza/W dół.gif");
         }
-
-        #region Obsluga zdarzeń
-        private void PictureBoxPraweMenuEkwipunek_MouseClick(object sender, EventArgs e)
-        {
-           // ekranEkwipunekTlo.ShowDialog();
-        }
-        private void PictureBoxPraweMenuEkwipunek_MouseEnter(object sender, EventArgs e)
-        {
-            Program.UstawObrazZDopasowaniemWielkosciObrazuDoKontrolki(praweMenu[0], "Resources/Grafiki menu/Alight.png");
-        }
-        private void PictureBoxPraweMenuEkwipunek_MouseLeave(object sender, EventArgs e)
-        {
-            Program.UstawObrazZDopasowaniemWielkosciObrazuDoKontrolki(praweMenu[0], "Resources/Grafiki menu/Adark.png");
-        }
-        private void PictureBoxPraweMenuEkranDziennikZadan_MouseClick(object sender, EventArgs e)
-        {
-           // ekranEkranDziennikZadanTlo.ShowDialog();
-        }
-
-        private void PictureBox_Click(object sender, EventArgs e)
-        {
-            //ekranEkranDziennikZadanTlo.ShowDialog();
-        }
-        #endregion
 
         public void RuchPowierzchniMapy(int pozycja, int dystans)
         {
