56729af1605a0190772060c2902abddb7105ca59
Shattan <rosiak90@gmail.com>
2015-11-02 12:56:58 +0100

    Edycja pola walki i normalizacja postaci walczących

--- RPG/Formy/EkranWalka.Designer.cs
+++ RPG/Formy/EkranWalka.Designer.cs
@@ -30,12 +30,35 @@
         {
             this.buttonWygralem = new System.Windows.Forms.Button();
             this.buttonPrzegralem = new System.Windows.Forms.Button();
+            this.LabelInformacje = new System.Windows.Forms.Label();
+            this.PictureBoxUcieczka = new System.Windows.Forms.PictureBox();
+            this.PictureBoxAtakMagiczny = new System.Windows.Forms.PictureBox();
+            this.PictureBoxEkwipunek = new System.Windows.Forms.PictureBox();
+            this.PictureBoxAtakFizyczny = new System.Windows.Forms.PictureBox();
+            this.FlowLayoutPanelWyborAtakuFizycznego = new System.Windows.Forms.FlowLayoutPanel();
+            this.PictureBoxWyjdzZAtakowFizycznych = new System.Windows.Forms.PictureBox();
+            this.FlowLayoutPanelWyborAtakuMagicznego = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBoxWyjdzZAtakowMagicznych = new System.Windows.Forms.PictureBox();
+            this.FlowLayoutPanelWyborMikstury = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBoxWyjdzZEkwipunku = new System.Windows.Forms.PictureBox();
+            this.FlowLayoutPanelWyboru = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxUcieczka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAtakMagiczny)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxEkwipunek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAtakFizyczny)).BeginInit();
+            this.FlowLayoutPanelWyborAtakuFizycznego.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxWyjdzZAtakowFizycznych)).BeginInit();
+            this.FlowLayoutPanelWyborAtakuMagicznego.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWyjdzZAtakowMagicznych)).BeginInit();
+            this.FlowLayoutPanelWyborMikstury.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWyjdzZEkwipunku)).BeginInit();
+            this.FlowLayoutPanelWyboru.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonWygralem
             // 
             this.buttonWygralem.BackColor = System.Drawing.Color.Lime;
-            this.buttonWygralem.Location = new System.Drawing.Point(30, 185);
+            this.buttonWygralem.Location = new System.Drawing.Point(40, 12);
             this.buttonWygralem.Name = "buttonWygralem";
             this.buttonWygralem.Size = new System.Drawing.Size(178, 69);
             this.buttonWygralem.TabIndex = 0;
@@ -46,7 +69,7 @@
             // buttonPrzegralem
             // 
             this.buttonPrzegralem.BackColor = System.Drawing.Color.Red;
-            this.buttonPrzegralem.Location = new System.Drawing.Point(30, 272);
+            this.buttonPrzegralem.Location = new System.Drawing.Point(224, 12);
             this.buttonPrzegralem.Name = "buttonPrzegralem";
             this.buttonPrzegralem.Size = new System.Drawing.Size(178, 69);
             this.buttonPrzegralem.TabIndex = 1;
@@ -54,12 +77,149 @@
             this.buttonPrzegralem.UseVisualStyleBackColor = false;
             this.buttonPrzegralem.Click += new System.EventHandler(this.buttonPrzegralem_Click);
             // 
+            // LabelInformacje
+            // 
+            this.LabelInformacje.BackColor = System.Drawing.Color.Black;
+            this.LabelInformacje.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelInformacje.ForeColor = System.Drawing.Color.Red;
+            this.LabelInformacje.Location = new System.Drawing.Point(409, 450);
+            this.LabelInformacje.Name = "LabelInformacje";
+            this.LabelInformacje.Size = new System.Drawing.Size(393, 128);
+            this.LabelInformacje.TabIndex = 2;
+            this.LabelInformacje.Text = "LabelInformacje";
+            this.LabelInformacje.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PictureBoxUcieczka
+            // 
+            this.PictureBoxUcieczka.BackColor = System.Drawing.Color.White;
+            this.PictureBoxUcieczka.Location = new System.Drawing.Point(3, 114);
+            this.PictureBoxUcieczka.Name = "PictureBoxUcieczka";
+            this.PictureBoxUcieczka.Size = new System.Drawing.Size(379, 31);
+            this.PictureBoxUcieczka.TabIndex = 3;
+            this.PictureBoxUcieczka.TabStop = false;
+            this.PictureBoxUcieczka.Click += new System.EventHandler(this.PictureBoxUcieczka_Click);
+            // 
+            // PictureBoxAtakMagiczny
+            // 
+            this.PictureBoxAtakMagiczny.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.PictureBoxAtakMagiczny.Location = new System.Drawing.Point(3, 40);
+            this.PictureBoxAtakMagiczny.Name = "PictureBoxAtakMagiczny";
+            this.PictureBoxAtakMagiczny.Size = new System.Drawing.Size(379, 31);
+            this.PictureBoxAtakMagiczny.TabIndex = 2;
+            this.PictureBoxAtakMagiczny.TabStop = false;
+            this.PictureBoxAtakMagiczny.Click += new System.EventHandler(this.PictureBoxAtakMagiczny_Click);
+            // 
+            // PictureBoxEkwipunek
+            // 
+            this.PictureBoxEkwipunek.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.PictureBoxEkwipunek.Location = new System.Drawing.Point(3, 77);
+            this.PictureBoxEkwipunek.Name = "PictureBoxEkwipunek";
+            this.PictureBoxEkwipunek.Size = new System.Drawing.Size(379, 31);
+            this.PictureBoxEkwipunek.TabIndex = 1;
+            this.PictureBoxEkwipunek.TabStop = false;
+            this.PictureBoxEkwipunek.Click += new System.EventHandler(this.PictureBoxEkwipunek_Click);
+            // 
+            // PictureBoxAtakFizyczny
+            // 
+            this.PictureBoxAtakFizyczny.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.PictureBoxAtakFizyczny.Location = new System.Drawing.Point(3, 3);
+            this.PictureBoxAtakFizyczny.Name = "PictureBoxAtakFizyczny";
+            this.PictureBoxAtakFizyczny.Size = new System.Drawing.Size(379, 31);
+            this.PictureBoxAtakFizyczny.TabIndex = 0;
+            this.PictureBoxAtakFizyczny.TabStop = false;
+            this.PictureBoxAtakFizyczny.Click += new System.EventHandler(this.PictureBoxAtakFizyczny_Click);
+            // 
+            // FlowLayoutPanelWyborAtakuFizycznego
+            // 
+            this.FlowLayoutPanelWyborAtakuFizycznego.AutoScroll = true;
+            this.FlowLayoutPanelWyborAtakuFizycznego.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.FlowLayoutPanelWyborAtakuFizycznego.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FlowLayoutPanelWyborAtakuFizycznego.Controls.Add(this.PictureBoxWyjdzZAtakowFizycznych);
+            this.FlowLayoutPanelWyborAtakuFizycznego.Location = new System.Drawing.Point(19, 87);
+            this.FlowLayoutPanelWyborAtakuFizycznego.Name = "FlowLayoutPanelWyborAtakuFizycznego";
+            this.FlowLayoutPanelWyborAtakuFizycznego.Size = new System.Drawing.Size(386, 177);
+            this.FlowLayoutPanelWyborAtakuFizycznego.TabIndex = 0;
+            this.FlowLayoutPanelWyborAtakuFizycznego.Visible = false;
+            // 
+            // PictureBoxWyjdzZAtakowFizycznych
+            // 
+            this.PictureBoxWyjdzZAtakowFizycznych.BackColor = System.Drawing.Color.Red;
+            this.PictureBoxWyjdzZAtakowFizycznych.Location = new System.Drawing.Point(3, 3);
+            this.PictureBoxWyjdzZAtakowFizycznych.Name = "PictureBoxWyjdzZAtakowFizycznych";
+            this.PictureBoxWyjdzZAtakowFizycznych.Size = new System.Drawing.Size(379, 31);
+            this.PictureBoxWyjdzZAtakowFizycznych.TabIndex = 0;
+            this.PictureBoxWyjdzZAtakowFizycznych.TabStop = false;
+            this.PictureBoxWyjdzZAtakowFizycznych.Click += new System.EventHandler(this.PictureBoxWyjdzZAtakowFizycznych_Click);
+            // 
+            // FlowLayoutPanelWyborAtakuMagicznego
+            // 
+            this.FlowLayoutPanelWyborAtakuMagicznego.AutoScroll = true;
+            this.FlowLayoutPanelWyborAtakuMagicznego.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.FlowLayoutPanelWyborAtakuMagicznego.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FlowLayoutPanelWyborAtakuMagicznego.Controls.Add(this.pictureBoxWyjdzZAtakowMagicznych);
+            this.FlowLayoutPanelWyborAtakuMagicznego.Location = new System.Drawing.Point(411, 87);
+            this.FlowLayoutPanelWyborAtakuMagicznego.Name = "FlowLayoutPanelWyborAtakuMagicznego";
+            this.FlowLayoutPanelWyborAtakuMagicznego.Size = new System.Drawing.Size(386, 177);
+            this.FlowLayoutPanelWyborAtakuMagicznego.TabIndex = 1;
+            this.FlowLayoutPanelWyborAtakuMagicznego.Visible = false;
+            // 
+            // pictureBoxWyjdzZAtakowMagicznych
+            // 
+            this.pictureBoxWyjdzZAtakowMagicznych.BackColor = System.Drawing.Color.Red;
+            this.pictureBoxWyjdzZAtakowMagicznych.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxWyjdzZAtakowMagicznych.Name = "pictureBoxWyjdzZAtakowMagicznych";
+            this.pictureBoxWyjdzZAtakowMagicznych.Size = new System.Drawing.Size(379, 31);
+            this.pictureBoxWyjdzZAtakowMagicznych.TabIndex = 0;
+            this.pictureBoxWyjdzZAtakowMagicznych.TabStop = false;
+            this.pictureBoxWyjdzZAtakowMagicznych.Click += new System.EventHandler(this.pictureBoxWyjdzZAtakowMagicznych_Click);
+            // 
+            // FlowLayoutPanelWyborMikstury
+            // 
+            this.FlowLayoutPanelWyborMikstury.AutoScroll = true;
+            this.FlowLayoutPanelWyborMikstury.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.FlowLayoutPanelWyborMikstury.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FlowLayoutPanelWyborMikstury.Controls.Add(this.pictureBoxWyjdzZEkwipunku);
+            this.FlowLayoutPanelWyborMikstury.Location = new System.Drawing.Point(411, 270);
+            this.FlowLayoutPanelWyborMikstury.Name = "FlowLayoutPanelWyborMikstury";
+            this.FlowLayoutPanelWyborMikstury.Size = new System.Drawing.Size(386, 177);
+            this.FlowLayoutPanelWyborMikstury.TabIndex = 1;
+            this.FlowLayoutPanelWyborMikstury.Visible = false;
+            // 
+            // pictureBoxWyjdzZEkwipunku
+            // 
+            this.pictureBoxWyjdzZEkwipunku.BackColor = System.Drawing.Color.Red;
+            this.pictureBoxWyjdzZEkwipunku.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxWyjdzZEkwipunku.Name = "pictureBoxWyjdzZEkwipunku";
+            this.pictureBoxWyjdzZEkwipunku.Size = new System.Drawing.Size(379, 31);
+            this.pictureBoxWyjdzZEkwipunku.TabIndex = 0;
+            this.pictureBoxWyjdzZEkwipunku.TabStop = false;
+            this.pictureBoxWyjdzZEkwipunku.Click += new System.EventHandler(this.pictureBoxWyjdzZEkwipunku_Click);
+            // 
+            // FlowLayoutPanelWyboru
+            // 
+            this.FlowLayoutPanelWyboru.AutoScroll = true;
+            this.FlowLayoutPanelWyboru.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.FlowLayoutPanelWyboru.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FlowLayoutPanelWyboru.Controls.Add(this.PictureBoxAtakFizyczny);
+            this.FlowLayoutPanelWyboru.Controls.Add(this.PictureBoxAtakMagiczny);
+            this.FlowLayoutPanelWyboru.Controls.Add(this.PictureBoxEkwipunek);
+            this.FlowLayoutPanelWyboru.Controls.Add(this.PictureBoxUcieczka);
+            this.FlowLayoutPanelWyboru.Location = new System.Drawing.Point(16, 270);
+            this.FlowLayoutPanelWyboru.Name = "FlowLayoutPanelWyboru";
+            this.FlowLayoutPanelWyboru.Size = new System.Drawing.Size(386, 177);
+            this.FlowLayoutPanelWyboru.TabIndex = 1;
+            // 
             // EkranWalka
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(970, 780);
+            this.Controls.Add(this.FlowLayoutPanelWyborAtakuFizycznego);
+            this.Controls.Add(this.FlowLayoutPanelWyborAtakuMagicznego);
+            this.Controls.Add(this.FlowLayoutPanelWyborMikstury);
+            this.Controls.Add(this.FlowLayoutPanelWyboru);
+            this.Controls.Add(this.LabelInformacje);
             this.Controls.Add(this.buttonPrzegralem);
             this.Controls.Add(this.buttonWygralem);
             this.Cursor = System.Windows.Forms.Cursors.Default;
@@ -69,6 +229,17 @@
             this.Text = "Walka";
             this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxUcieczka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAtakMagiczny)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxEkwipunek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAtakFizyczny)).EndInit();
+            this.FlowLayoutPanelWyborAtakuFizycznego.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxWyjdzZAtakowFizycznych)).EndInit();
+            this.FlowLayoutPanelWyborAtakuMagicznego.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWyjdzZAtakowMagicznych)).EndInit();
+            this.FlowLayoutPanelWyborMikstury.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWyjdzZEkwipunku)).EndInit();
+            this.FlowLayoutPanelWyboru.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -77,5 +248,17 @@
 
         private System.Windows.Forms.Button buttonWygralem;
         private System.Windows.Forms.Button buttonPrzegralem;
+        private System.Windows.Forms.Label LabelInformacje;
+        private System.Windows.Forms.PictureBox PictureBoxUcieczka;
+        private System.Windows.Forms.PictureBox PictureBoxAtakMagiczny;
+        private System.Windows.Forms.PictureBox PictureBoxEkwipunek;
+        private System.Windows.Forms.PictureBox PictureBoxAtakFizyczny;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayoutPanelWyborAtakuFizycznego;
+        private System.Windows.Forms.PictureBox PictureBoxWyjdzZAtakowFizycznych;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayoutPanelWyborAtakuMagicznego;
+        private System.Windows.Forms.PictureBox pictureBoxWyjdzZAtakowMagicznych;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayoutPanelWyborMikstury;
+        private System.Windows.Forms.PictureBox pictureBoxWyjdzZEkwipunku;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayoutPanelWyboru;
     }
 }
--- RPG/Formy/EkranWalka.cs
+++ RPG/Formy/EkranWalka.cs
@@ -20,14 +20,51 @@ namespace RPG
         {
             this.ekranGry = ekranGry;
             InitializeComponent();
-            UstawElementyNaEkranie();
+            RozmiescElementy();
         }
 
         #region Metody
-        void UstawElementyNaEkranie()
+        void RozmiescElementy()
         {
             //Ustawienia okienka gry
             Program.DopasujRozmiarFormyDoEkranu(this);
+
+            //Ustawienie przycisków
+            Program.UstawObrazZDopasowaniemWielkosciKontrolkiDoObrazu(PictureBoxUcieczka,"Resources/Grafiki menu/Przykładowy przycisk.png");
+
+            foreach (PictureBox przycisk in FlowLayoutPanelWyboru.Controls)
+            {
+                przycisk.Size = PictureBoxUcieczka.Size;
+            }
+            foreach (PictureBox przycisk in FlowLayoutPanelWyborAtakuFizycznego.Controls)
+            {
+                przycisk.Size = PictureBoxUcieczka.Size;
+            }
+            foreach (PictureBox przycisk in FlowLayoutPanelWyborAtakuMagicznego.Controls)
+            {
+                przycisk.Size = PictureBoxUcieczka.Size;
+            }
+            foreach (PictureBox przycisk in FlowLayoutPanelWyborMikstury.Controls)
+            {
+                przycisk.Size = PictureBoxUcieczka.Size;
+            }
+
+            //Ustawienie FlowLayoutPaneli
+            FlowLayoutPanelWyboru.Size = new Size(FlowLayoutPanelWyboru.Controls[0].Width+20, FlowLayoutPanelWyboru.Controls.Count * (PictureBoxUcieczka.Height + 6));
+            FlowLayoutPanelWyborAtakuFizycznego.Size = FlowLayoutPanelWyboru.Size;
+            FlowLayoutPanelWyborAtakuMagicznego.Size = FlowLayoutPanelWyboru.Size;
+            FlowLayoutPanelWyborMikstury.Size = FlowLayoutPanelWyboru.Size;
+
+            FlowLayoutPanelWyboru.Location = new Point(0, Height-FlowLayoutPanelWyboru.Height);
+            FlowLayoutPanelWyborAtakuFizycznego.Location = FlowLayoutPanelWyboru.Location;
+            FlowLayoutPanelWyborAtakuMagicznego.Location = FlowLayoutPanelWyboru.Location;
+            FlowLayoutPanelWyborMikstury.Location = FlowLayoutPanelWyboru.Location;
+
+
+            //Ustawienia dolnego panelu z informacjami
+            LabelInformacje.Size = new Size(Width -FlowLayoutPanelWyboru.Width, FlowLayoutPanelWyboru.Height);
+            LabelInformacje.Location = new Point(Width-LabelInformacje.Width, Height - LabelInformacje.Height);
+            LabelInformacje.Text = "Rozpoczęłą się walka!";
 
             //Ustawienie ikony w trybie okienkowym
             Icon = new Icon("Resources/Grafiki menu/Ikona.ico");
@@ -50,6 +87,62 @@ namespace RPG
         {
             DialogResult = DialogResult.Cancel;
         }
+
+        #region Panel początkowy tury
+        private void PictureBoxAtakFizyczny_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanelWyborAtakuFizycznego.Visible = true;
+        }
+
+        private void PictureBoxAtakMagiczny_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanelWyborAtakuMagicznego.Visible = true;
+        }
+
+        private void PictureBoxEkwipunek_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanelWyborMikstury.Visible = true;
+        }
+
+        private void PictureBoxUcieczka_Click(object sender, EventArgs e)
+        {
+            Random Losuj = new Random();
+            int wylosowana = Losuj.Next(0, 100);
+
+            if (wylosowana < 50)
+            {
+                LabelInformacje.Text = "Próba ucieczki nieudana!";
+            }
+            else
+            {
+                LabelInformacje.Text = "Udało Ci się uciec!";
+                //DialogResult = DialogResult.Ignore;
+            }
+        }
+        #endregion
+
+        #region Panel ataków fizycznych
+        private void PictureBoxWyjdzZAtakowFizycznych_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanelWyborAtakuFizycznego.Visible = false;
+        }
+        #endregion
+
+        #region Panel ataków magicznych
+        private void pictureBoxWyjdzZAtakowMagicznych_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanelWyborAtakuMagicznego.Visible = false;
+        }
+        #endregion
+
+        #region Panel ekwipunku
+        private void pictureBoxWyjdzZEkwipunku_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanelWyborMikstury.Visible = false;
+        }
+        #endregion
+
+
         /*
         #region sprawiamy, ze okno jest niewidoczne w alt+tab
         //Obsluga wychodzenia - zakaz alt+f4
