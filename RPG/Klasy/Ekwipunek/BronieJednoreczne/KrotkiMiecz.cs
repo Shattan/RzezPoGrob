a007e2fa8c676977735c28ccfcd564433224c674
harorri <harorri@gmail.com>
2015-12-08 22:46:27 +0100

    Revert "Revert "d""
    This reverts commit dbdf6099d077d07580be2a065a1f2f1f726476df.

---
+++ RPG/Klasy/Ekwipunek/BronieJednoreczne/KrotkiMiecz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Ekwipunek.BronieJednoreczne
+{
+    public class KrotkiMiecz:RPG.Ekwipunek
+    {
+        public override string Nazwa
+        {
+            get { return "Krótki miecz"; }
+        }
+
+        public override string Obrazek
+        {
+            get { return "Resources/Grafiki ekwipunku/bron1hMieczProsty.png"; }
+        }
+        public override double Obrazenia
+        {
+            get
+            {
+                return 5;
+            }
+        }
+        public override int Sila
+        {
+            get
+            {
+                return 3;
+            }
+        }
+        public override TypPrzedmiotu TypPrzedmiotu
+        {
+            get { return Klasy.TypPrzedmiotu.BronJednoreczna ; }
+        }
+    }
+}
---
+++ RPG/Klasy/Ekwipunek/BronieJednoreczne/NozDoMasla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Ekwipunek.BronieJednoreczne
+{
+    public class NozDoMasla:RPG.Ekwipunek
+    {
+        public override string Nazwa
+        {
+            get { return "Nóż do masła"; }
+        }
+
+        public override string Obrazek
+        {
+            get { return "Resources/Grafiki ekwipunku/bron1hNóż do masła.png"; }
+        }
+
+        public override int Sila
+        {
+            get
+            {
+                return 1;
+            }
+        }
+        public override int Zrecznosc
+        {
+            get
+            {
+                return 1;
+            }
+        }
+        public override TypPrzedmiotu TypPrzedmiotu
+        {
+            get { return Klasy.TypPrzedmiotu.BronJednoreczna; }
+        }
+    }
+}
