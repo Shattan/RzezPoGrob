f257af3ad8953ebf239bf8d512be799c9ec47863
harorri <harorri@gmail.com>
2015-12-11 01:13:17 +0100

    domek
    Domek z 6 czesci

---
+++ RPG/Klasy/Obiekty/dom1_3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Obiekty
+{
+    class dom1_3 : Przeszkoda
+    {
+        public override string ObrazekNaMapie
+        {
+            get { return "Resources/Mapy/dom1_3.png"; }
+        }
+    }
+}
---
+++ RPG/Klasy/Obiekty/dom1_5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Obiekty
+{
+    class dom1_5 : Przeszkoda
+    {
+        public override string ObrazekNaMapie
+        {
+            get { return "Resources/Mapy/dom1_5.png"; }
+        }
+    }
+}
