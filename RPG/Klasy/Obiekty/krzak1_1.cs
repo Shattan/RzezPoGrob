ef245121414bfe3bc6eba9319ca9d5fbd23d2e80
harorri <harorri@gmail.com>
2015-12-12 17:20:12 +0100

    Elementy mapy dalejj

---
+++ RPG/Klasy/Obiekty/krzak1_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Obiekty
+{
+    class krzak1_1 : Przeszkoda
+    {
+        public override string ObrazekNaMapie
+        {
+            get { return "Resources/Mapy/Krzak1_1.png"; }
+        }
+    }
+}
---
+++ RPG/Klasy/Obiekty/krzak2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Obiekty
+{
+    class krzak2 : Przeszkoda
+    {
+        public override string ObrazekNaMapie
+        {
+            get { return "Resources/Mapy/Krzak2.png"; }
+        }
+    }
+}
