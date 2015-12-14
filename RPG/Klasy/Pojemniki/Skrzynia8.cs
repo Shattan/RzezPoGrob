fd30fc7c38c3c05f938c732316b3984011db0d67
Maggus85 <mateusz.pniewski@gmail.com>
2015-12-14 23:41:13 +0100

    skrzynki na mapie

---
+++ RPG/Klasy/Pojemniki/Skrzynia8.cs
@@ -0,0 +1,25 @@
+﻿using RPG.Klasy.Ekwipunek.BronieJednoreczne;
+using RPG.Klasy.Ekwipunek.Zywnosc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Pojemniki
+{
+    public class Skrzynia8 : PojemnikBaza
+    {
+        protected override List<RPG.Ekwipunek> Przedmioty
+        {
+            get { return new List<RPG.Ekwipunek>() {new SwiezyChleb() }; }
+        }
+        protected override int Zloto
+        {
+            get
+            {
+                return 10;
+            }
+        }
+    }
+}
