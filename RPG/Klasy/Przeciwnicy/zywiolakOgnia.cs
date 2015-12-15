12bf5ea52afc9b3491ccdfdb6d244f8490444185
harorri <harorri@gmail.com>
2015-12-15 13:37:35 +0100

    an

---
+++ RPG/Klasy/Przeciwnicy/zywiolakOgnia.cs
@@ -0,0 +1,43 @@
+﻿using RPG.Klasy.Umiejetnosci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Przeciwnicy
+{
+    public class zywiolakOgnia : Przeciwnik
+    {
+        public override int Sila { get { return 7; } }
+        public override int Zrecznosc { get { return 4; } }
+        public override int Witalnosc { get { return 6; } }
+        public override int Inteligencja { get { return 2; } }
+        public override double Obrazenia { get { return 7; } }
+        public override double Pancerz { get { return 3; } }
+        public override double HP { get { return 10; } }
+        public override double Energia { get { return 10; } }
+        public override double SzansaNaTrafienie { get { return 30; } }
+        public override double SzansaNaKrytyczne { get { return 5; } }
+
+        public override string Nazwa
+        {
+            get { return "Żywiołak ognia"; }
+        }
+
+        public override string ObrazekWalki
+        {
+            get { return "Resources/Grafiki postaci walczących/zywiolakOgnia.png"; }
+        }
+
+        public override string ObrazekNaMapie
+        {
+            get { return "Resources/Grafiki postaci walczących/zywiolak ognia.png"; }
+        }
+
+        public override List<Umiejetnosc> Umiejetnosci()
+        {
+            return new List<Umiejetnosc> { new Wymachiwanie() };
+        }
+    }
+}
---
+++ RPG/Klasy/Przeciwnicy/zywiolakWody.cs
@@ -0,0 +1,45 @@
+﻿using RPG.Klasy.Umiejetnosci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Klasy.Przeciwnicy
+{
+    public class zywiolakWody : Przeciwnik
+    {
+        public override int Sila { get { return 7; } }
+        public override int Zrecznosc { get { return 2; } }
+        public override int Witalnosc { get { return 8; } }
+        public override int Inteligencja { get { return 2; } }
+        public override double Obrazenia { get { return 7; } }
+        public override double Pancerz { get { return 3; } }
+        public override double HP { get { return 10; } }
+        public override double Energia { get { return 10; } }
+        public override double SzansaNaTrafienie { get { return 30; } }
+        public override double SzansaNaKrytyczne { get { return 5; } }
+
+        public override string Nazwa
+        {
+            get { return "Zywiołak wody"; }
+        }
+
+        public override string ObrazekWalki
+        {
+            get { return "Resources/Grafiki postaci walczących/zywiolakWody.png"; }
+        }
+
+        public override string ObrazekNaMapie
+        {
+            get { return "Resources/Grafiki postaci walczących/zywiolak wody.png"; }
+        }
+
+        public override List<Umiejetnosc> Umiejetnosci()
+        {
+            return new List<Umiejetnosc> { new Wymachiwanie() };
+        }
+    }
+}
+
+
